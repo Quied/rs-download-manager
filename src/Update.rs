7320012a3503b82c341471faa8714f6a92d6c5cf
Quied <quiedcpp@gmail.com>
2024-02-04 22:55:42 +0200

    download, maybe replace

--- src/Update.rs
+++ src/Update.rs
@@ -1,76 +1,156 @@
-// use self_update::cargo_crate_version;
-
-
 pub mod update {
-
-    use egui::accesskit::kurbo::Arc;
-    use reqwest::header::STRICT_TRANSPORT_SECURITY;
-    // use crate::Update::cargo_crate_version;
     use self_update::cargo_crate_version;
     use self_update::self_replace;
-    use self_update::update::Release;
-    use self_update::update::ReleaseUpdate;
-    use std::env::consts::EXE_EXTENSION;
-    use std::env::current_exe;
-    // let bin_name = std::path::PathBuf::from("this_temp_name");
+    use std::io::{stdin, Read};
+    use std::{fs, io};
 
-    pub fn check_rel_list() -> Result<(), Box<dyn (::std::error::Error)>> {
-        let mut builds = self_update::backends::github::ReleaseList::configure();
-        println!("checking on realeses...");
+    use self_update::{Download, Extract};
+    use std::fs::{set_permissions, File, OpenOptions};
+    use std::path::Path;
 
-        #[cfg(feature = "signatures")]
-        builds.repo_owner("jaemk");
-
-        let releases = builds.repo_name("self_update").build()?.fetch()?;
-        //        let releases = builds.repo_name("self_updates").build()?.fetch()?;
-        println!("releases: ");
-        println!("{:#?}\n", releases);
-
-        Ok(())
-    }
-
-    pub fn replace_itself() {
-        let APP_NAME = "Achernar".to_owned();
-
-        let exe = std::env::current_exe().unwrap();
-        let new_executable = std::fs::read_link(exe.clone())
-            .unwrap_or(exe)
-            .with_file_name(APP_NAME.clone())
-            .with_extension(EXE_EXTENSION);
-
-        if !new_executable.is_file() {
-            eprintln!("hello does not exist, run cargo build --example hello first.");
-            std::process::exit(1);
-        }
-
-        println!("Next time I run, I am the {} executable", APP_NAME);
-        self_replace::self_replace(&new_executable).unwrap();
-
-        if std::env::var("FORCE_EXIT").ok().as_deref() == Some("1") {
-            std::process::exit(0);
-        }
-    }
-
-    pub fn check_status(
-        new_version: &mut String,
-        is_new_upd: &mut bool,
-    ) -> Result<(), Box<dyn (::std::error::Error)>> {
+    pub fn check_updates() -> Result<(), Box<dyn (::std::error::Error)>> {
         let status = self_update::backends::github::Update::configure()
             .repo_owner("quied")
             .repo_name("rs-download-manager")
             .bin_name("github")
-            .no_confirm(true)
             .show_download_progress(true)
             .current_version(cargo_crate_version!())
             .build()?
             .update()?;
+
         println!("Update status: `{}`!", status.version());
-
-        *new_version = String::from(status.version());
-        *is_new_upd = true;
-
         Ok(())
     }
+
+    pub fn download_updates() -> Result<(), Box<dyn std::error::Error>> {
+        let releases = self_update::backends::github::ReleaseList::configure()
+            // .repo_owner("jaemk")
+            // .repo_name("self_update")
+            .repo_owner("quied")
+            .repo_name("rs-download-manager")
+            .build()?
+            .fetch()?;
+        println!("found releases:");
+        println!("{:#?}\n", releases);
+
+        let asset = releases[0]
+            .asset_for(&self_update::get_target(), None)
+            .unwrap();
+
+        let tmp_dir = tempfile::Builder::new()
+            .prefix("new_dir")
+            .tempdir_in(::std::env::current_dir()?)?;
+
+        let static_dir = std::path::Path::new("/temp/");
+
+        println!("[tmp_dir] {:?}", tmp_dir);
+
+        let tmp_dir_path = tmp_dir.path();
+
+        if fs::metadata(tmp_dir_path).is_ok() {
+            println!("Temporary directory exists: {:?}", tmp_dir_path);
+        } else {
+            println!("Temporary directory does not exist: {:?}", tmp_dir_path);
+        }
+
+        let tmp_dir_path = "./static_dir/";
+
+        // Attempt to open the directory
+        match File::open(&tmp_dir_path) {
+            Ok(file) => {
+                println!("Path exists, but it is a file: {:?}", tmp_dir_path);
+            }
+            Err(_) => {
+                // Check if the directory exists
+                if fs::metadata(&tmp_dir_path).is_ok() {
+                    println!("Directory exists: {:?}", tmp_dir_path);
+                } else {
+                    println!("Directory does not exist: {:?}", tmp_dir_path);
+                }
+            }
+        }
+
+        let tmp_tarball_path = tmp_dir.path().join(&asset.name);
+        //let tmp_tarball_path = dir_name.path().join(&asset.name);
+
+        println!("[tmp_tarball_path] {:?}", tmp_tarball_path);
+        println!("[ASSET NAME] {:?}", &asset.name);
+
+        let file_path = String::from("./static_dir/".to_owned() + &asset.name);
+
+        let tmp_tarball = ::std::fs::File::create(&file_path)?;
+
+        println!("[tmp_tarball] {:?}", tmp_tarball);
+
+        self_update::Download::from_url(&asset.download_url)
+            .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
+            .download_to(&tmp_tarball)?;
+
+        println!("5");
+
+        let extract_target = std::path::Path::new(&file_path);
+        let file_for_save = std::path::Path::new("./static_dir/");
+        let bin_name = std::path::PathBuf::from("target_bin_file");
+
+        println!("[extract_target]: {:?}", extract_target);
+        println!("[bin_name]: {:?}", bin_name);
+        println!("[tmp_dir]: {:?}", tmp_dir.path());
+        println!("[file_for_save]: {:?}", file_for_save);
+
+        println!("6");
+
+        //self_update::Extract::from_source(&tmp_tarball_path)
+        self_update::Extract::from_source(&extract_target)
+            .archive(self_update::ArchiveKind::Plain(Some(
+                self_update::Compression::Gz,
+            )))
+            .extract_file(&file_for_save, &bin_name)?;
+
+        let new_exe = file_for_save.join(bin_name);
+        println!("[new_exe]: {:?}", new_exe);
+        self_replace::self_replace(new_exe)?;
+
+        try_decompress_tar(extract_target);
+        println!("end");
+        Ok(())
+    }
+
+    pub fn try_decompress_tar(target: &Path) -> Result<(), Box<dyn std::error::Error>> {
+        use flate2::read::GzDecoder;
+        use tar::Archive;
+
+        let path = target.clone();
+        println!("[decompress target] {:?}", target);
+
+        let tar_gz = File::open(path)?;
+        let tar = GzDecoder::new(tar_gz);
+        let mut archive = Archive::new(tar);
+        archive.unpack(".")?;
+
+        println!("[success decompressed] {:?}", target);
+        Ok(())
+    }
+
+    // pub fn check_status(
+    //     new_version: &mut String,
+    //     is_new_upd: &mut bool,
+    // ) -> Result<(), Box<dyn (::std::error::Error)>> {
+    //     let status = self_update::backends::github::Update::configure()
+    //         .repo_owner("quied")
+    //         .repo_name("rs-download-manager")
+    //         .bin_name("github")
+    //         .no_confirm(true)
+    //         .show_download_progress(true)
+    //         .current_version(cargo_crate_version!())
+    //         .build()?
+    //         .update()?;
+    //     println!("Update status: `{}`!", status.version());
+
+    //     *new_version = String::from(status.version());
+    //     *is_new_upd = true;
+
+    //     Ok(())
+    // }
 
     // pub fn get_updates() -> Result<(), Box<dyn (::std::error::Error)>> {
 
@@ -88,150 +168,151 @@ pub mod update {
     // }
 
     // GPT variant
-    pub fn get_release_this() -> Result<(), Box<dyn std::error::Error>> {
-        use std::fs::File;
-        use std::io::prelude::*;
+    // pub fn get_release_this() -> Result<(), Box<dyn std::error::Error>> {
+    //     use std::fs::File;
+    //     use std::io::prelude::*;
 
-        extern crate tar;
+    //     extern crate tar;
 
-        let releases = self_update::backends::github::ReleaseList::configure()
-            .repo_owner("quied")
-            .repo_name("rs-download-manager")
-            .build()?
-            .fetch()?;
+    //     let releases = self_update::backends::github::ReleaseList::configure()
+    //         .repo_owner("quied")
+    //         .repo_name("rs-download-manager")
+    //         .build()?
+    //         .fetch()?;
 
-        println!("found releases:");
-        println!("{:#?}\n", releases);
+    //     println!("found releases:");
+    //     println!("{:#?}\n", releases);
 
-        let asset = releases[0].asset_for("achernar", None).unwrap();
+    //     let asset = releases[0].asset_for("achernar", None).unwrap();
 
-        let mut test = String::from("./test-folder/");
-        test.push_str(&asset.name);
+    //     let mut test = String::from("./test-folder/");
+    //     test.push_str(&asset.name);
 
-        let bin_name_with_path = File::create(&test);
+    //     let bin_name_with_path = File::create(&test);
 
-        // self_update::Download::from_url(&asset.download_url)
-        //     .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
-        //     .download_to(bin_name_with_path)?;
+    //     // self_update::Download::from_url(&asset.download_url)
+    //     //     .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
+    //     //     .download_to(bin_name_with_path)?;
 
-        // let bin_name = std::path::PathBuf::from("self_update_bin");
-        // self_update::Extract::from_source(&tmp_tarball_path)
-        //     .archive(self_update::ArchiveKind::Tar(Some(self_update::Compression::Gz)))
-        //     .extract_file(&tmp_dir.path(), &bin_name)?;
+    //     // let bin_name = std::path::PathBuf::from("self_update_bin");
+    //     // self_update::Extract::from_source(&tmp_tarball_path)
+    //     //     .archive(self_update::ArchiveKind::Tar(Some(self_update::Compression::Gz)))
+    //     //     .extract_file(&tmp_dir.path(), &bin_name)?;
 
-        Ok(())
-    }
+    //     Ok(())
+    // }
 
-    pub fn test_upd() -> Result<(), Box<dyn (::std::error::Error)>> {
-        //     let status = self_update::backends::github::Update::configure()
-        //         .repo_owner("andrew121410")
-        //         .repo_name("Limonium")
-        //         .bin_name("limonium")
-        //         .target("limonium")
-        //         .no_confirm(true)
-        //         .show_download_progress(true)
-        //         .show_output(true)
-        //         .current_version(cargo_crate_version!())
-        //         .build()?;
+    // pub fn test_upd() -> Result<(), Box<dyn (::std::error::Error)>> {
+    //     //     let status = self_update::backends::github::Update::configure()
+    //     //         .repo_owner("andrew121410")
+    //     //         .repo_name("Limonium")
+    //     //         .bin_name("limonium")
+    //     //         .target("limonium")
+    //     //         .no_confirm(true)
+    //     //         .show_download_progress(true)
+    //     //         .show_output(true)
+    //     //         .current_version(cargo_crate_version!())
+    //     //         .build()?;
 
-        //    println!("Update status: `{}`!", status.current_version());
-        //     Ok(())
+    //     //    println!("Update status: `{}`!", status.current_version());
+    //     //     Ok(())
 
-        // use std::io::prelude::*;
-        // use tar::Archive;
+    //     // use std::io::prelude::*;
+    //     // use tar::Archive;
 
-        //         let releases = self_update::backends::github::ReleaseList::configure()
-        //         .repo_owner("jaemk")
-        //         .repo_name("self_update")
-        //         .build()?
-        //         .fetch()?;
-        //     println!("found releases:");
-        //     println!("{:#?}\n", releases);
+    //     //         let releases = self_update::backends::github::ReleaseList::configure()
+    //     //         .repo_owner("jaemk")
+    //     //         .repo_name("self_update")
+    //     //         .build()?
+    //     //         .fetch()?;
+    //     //     println!("found releases:");
+    //     //     println!("{:#?}\n", releases);
 
-        //     // get the first available release
-        //     let asset = releases[0]
-        //         .asset_for(&self_update::get_target(), None).unwrap();
+    //     //     // get the first available release
+    //     //     let asset = releases[0]
+    //     //         .asset_for(&self_update::get_target(), None).unwrap();
 
-        //     let tmp_dir = tempfile::Builder::new()
-        //             .prefix("self_update")
-        //             .tempdir_in(::std::env::current_dir()?)?;
-        //     let tmp_tarball_path = tmp_dir.path().join(&asset.name);
-        //     let tmp_tarball = ::std::fs::File::open(&tmp_tarball_path)?;
+    //     //     let tmp_dir = tempfile::Builder::new()
+    //     //             .prefix("self_update")
+    //     //             .tempdir_in(::std::env::current_dir()?)?;
+    //     //     let tmp_tarball_path = tmp_dir.path().join(&asset.name);
+    //     //     let tmp_tarball = ::std::fs::File::open(&tmp_tarball_path)?;
 
-        //     self_update::Download::from_url(&asset.download_url)
-        //         .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
-        //         .download_to(&tmp_tarball)?;
+    //     //     self_update::Download::from_url(&asset.download_url)
+    //     //         .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
+    //     //         .download_to(&tmp_tarball)?;
 
-        //     let bin_name = std::path::PathBuf::from("self_update_bin");
-        //     self_update::Extract::from_source(&tmp_tarball_path)
-        //         .archive(self_update::ArchiveKind::Tar(Some(self_update::Compression::Gz)))
-        //         .extract_file(&tmp_dir.path(), &bin_name)?;
+    //     //     let bin_name = std::path::PathBuf::from("self_update_bin");
+    //     //     self_update::Extract::from_source(&tmp_tarball_path)
+    //     //         .archive(self_update::ArchiveKind::Tar(Some(self_update::Compression::Gz)))
+    //     //         .extract_file(&tmp_dir.path(), &bin_name)?;
 
-        //     let new_exe = tmp_dir.path().join(bin_name);
-        //     self_replace::self_replace(new_exe)?;
+    //     //     let new_exe = tmp_dir.path().join(bin_name);
+    //     //     self_replace::self_replace(new_exe)?;
 
-        //     Ok(())
-        let status = self_update::backends::github::Update::configure()
-            .repo_owner("quied")
-            .repo_name("rs-download-manager")
-            .bin_name("github")
-            .show_download_progress(true)
-            .current_version(cargo_crate_version!())
-            .no_confirm(true)
-            .build()?
-            .update()?;
-        println!("Update status: `{}`!", status.version());
-        Ok(())
-    }
+    //     //     Ok(())
+    //     let status = self_update::backends::github::Update::configure()
+    //         .repo_owner("jaemk")
+    //         .repo_name("self_update")
+    //         .bin_name("target")
+    //         .show_download_progress(true)
+    //         .current_version(cargo_crate_version!())
+    //         .no_confirm(true)
+    //         .build()?
+    //         .update()?;
+    //     println!("Update status: `{}`!", status.version());
 
-    pub fn get_releases_list() -> Result<Vec<Release>, Box<dyn (::std::error::Error)>> {
-        let releases = self_update::backends::github::ReleaseList::configure()
-            .repo_owner("quied")
-            .repo_name("rs-download-manager")
-            .build()?
-            .fetch()?;
-        println!("found releases:");
-        println!("[skip]");
+    //     Ok(())
+    // }
 
-        Ok(releases)
-    }
+    // pub fn get_releases_list() -> Result<Vec<Release>, Box<dyn (::std::error::Error)>> {
+    //     let releases = self_update::backends::github::ReleaseList::configure()
+    //         .repo_owner("quied")
+    //         .repo_name("rs-download-manager")
+    //         .build()?
+    //         .fetch()?;
+    //     println!("found releases:");
+    //     println!("[skip]");
 
-    pub fn update_current() -> Result<(), Box<dyn (::std::error::Error)>> {
-        let releases = self_update::backends::github::ReleaseList::configure()
-            .repo_owner("quied")
-            .repo_name("rs-download-manager")
-            .build()?
-            .fetch()?;
-        println!("found releases:");
-        println!("{:#?}\n", releases);
+    //     Ok(releases)
+    // }
 
-        let temp = Some("hell");
+    // pub fn update_current() -> Result<(), Box<dyn (::std::error::Error)>> {
+    //     let releases = self_update::backends::github::ReleaseList::configure()
+    //         .repo_owner("quied")
+    //         .repo_name("rs-download-manager")
+    //         .build()?
+    //         .fetch()?;
+    //     println!("found releases:");
+    //     println!("{:#?}\n", releases);
 
-        // get the first available release
-        let asset = releases[0]
-            .asset_for(&self_update::get_target(), temp)
-            .unwrap();
+    //     let temp = Some("hell");
 
-        // self_update::Download::from_url(url)
+    //     // get the first available release
+    //     let asset = releases[0]
+    //         .asset_for(&self_update::get_target(), temp)
+    //         .unwrap();
 
-        let tmp_dir = tempfile::Builder::new()
-            .prefix("self_update")
-            .tempdir_in(::std::env::current_dir()?)?;
-        let tmp_tarball_path = tmp_dir.path().join(&asset.name);
-        let tmp_tarball = ::std::fs::File::open(&tmp_tarball_path)?;
+    //     // self_update::Download::from_url(url)
 
-        self_update::Download::from_url(&asset.download_url)
-            .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
-            .download_to(&tmp_tarball)?;
+    //     let tmp_dir = tempfile::Builder::new()
+    //         .prefix("self_update")
+    //         .tempdir_in(::std::env::current_dir()?)?;
+    //     let tmp_tarball_path = tmp_dir.path().join(&asset.name);
+    //     let tmp_tarball = ::std::fs::File::open(&tmp_tarball_path)?;
 
-        let bin_name = std::path::PathBuf::from("self_update_bin");
-        self_update::Extract::from_source(&tmp_tarball_path)
-            // .archive(self_update::ArchiveKind::Tar(Some(self_update::Compression::Gz)))
-            .extract_file(&tmp_dir.path(), &bin_name)?;
+    //     self_update::Download::from_url(&asset.download_url)
+    //         .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
+    //         .download_to(&tmp_tarball)?;
 
-        let new_exe = tmp_dir.path().join(bin_name);
-        self_replace::self_replace(new_exe)?;
+    //     let bin_name = std::path::PathBuf::from("self_update_bin");
+    //     self_update::Extract::from_source(&tmp_tarball_path)
+    //         // .archive(self_update::ArchiveKind::Tar(Some(self_update::Compression::Gz)))
+    //         .extract_file(&tmp_dir.path(), &bin_name)?;
 
-        Ok(())
-    }
+    //     let new_exe = tmp_dir.path().join(bin_name);
+    //     self_replace::self_replace(new_exe)?;
+
+    //     Ok(())
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,7 @@ use webbrowser;
 mod Update;
 mod View;
 
-use Update::update::get_release_this;
-use Update::update::test_upd;
-use Update::update::update_current;
-use Update::update::{check_rel_list, check_status, get_releases_list, replace_itself};
+use Update::update::{check_updates, download_updates};
 
 use egui::widgets::ProgressBar;
 use std::fs;
@@ -41,49 +38,33 @@ impl Default for MyApp {
 }
 
 fn main() {
-    // Window
+    env_logger::init();
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(820.0, 540.0)),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([640.0, 240.0])
+            .with_drag_and_drop(true),
         ..Default::default()
     };
-    // Window Title
     eframe::run_native(
-        "Achernar",
+        "rs-downloader",
         options,
-        Box::new(|_cc| Box::new(MyApp::default())),
+        Box::new(|_cc| Box::<MyApp>::default()),
     )
+    .expect("msg");
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let APP_VERSION = "Current version 0.1.0".to_owned();
+        let APP_VERSION = "Current version 0.1.0".to_owned(); // HARD
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
                 if ui.add(egui::Button::new("Check updates")).clicked() {
-                    let _ = check_status(&mut self.available_version, &mut self.is_new_update);
-                    println!(
-                        "[self.available_version] {} [self.is_new_update] {}",
-                        self.available_version, self.is_new_update
-                    );
+                    let _ = check_updates();
                 }
 
-                if ui.add(egui::Button::new("Donwload updates")).clicked() {
-                    // get_release_this();
-                    test_upd();
-                }
-
-                if ui.add(egui::Button::new("Realeses")).clicked() {
-                    let _ = check_rel_list();
-                }
-
-                if ui.add(egui::Button::new("Get list all realeses")).clicked() {
-                    self.all_releases = get_releases_list().expect("message");
-                    println!("{:?}", self.all_releases);
-                }
-
-                if ui.add(egui::Button::new("Replace")).clicked() {
-                    replace_itself();
+                if ui.add(egui::Button::new("Download updates")).clicked() {
+                    let _ = download_updates();
                 }
             });
 
@@ -92,33 +73,33 @@ impl eframe::App for MyApp {
             ui.vertical(|ui| {
                 ui.add(egui::Label::new("List updates:"));
 
-                for release in &self.all_releases {
-                    let version = &release.version;
-                    ui.add(egui::Label::new(version.to_owned()));
-                }
+                // for release in &self.all_releases {
+                //     let version = &release.version;
+                //     ui.add(egui::Label::new(version.to_owned()));
+                // }
 
-                if ui.add(egui::Button::new("Refresh")).clicked() {
-                    self.all_releases = get_releases_list().expect("Error: ");
-                    println!("REFRESH: {:?}", self.all_releases);
-                }
+                // if ui.add(egui::Button::new("Refresh")).clicked() {
+                //     self.all_releases = get_releases_list().expect("Error: ");
+                //     println!("REFRESH: {:?}", self.all_releases);
+                // }
 
-                ui.separator();
+                // ui.separator();
 
-                // === SHOW VERSION ===
-                if !self.all_releases.is_empty() {
-                    let now_release = &self.all_releases.last();
-                    ui.add(egui::Label::new(&now_release.unwrap().version));
-                }
+                // // === SHOW VERSION ===
+                // if !self.all_releases.is_empty() {
+                //     let now_release = &self.all_releases.last();
+                //     ui.add(egui::Label::new(&now_release.unwrap().version));
+                // }
 
-                if ui.add(egui::Button::new("Update to last")).clicked() {
-                    let _ = update_current();
-                }
+                // if ui.add(egui::Button::new("Update to last")).clicked() {
+                //     let _ = update_current();
+                // }
             });
 
-            let new_release = "[NEW] Available version: ".to_owned() + &self.available_version;
-            if self.is_new_update {
-                ui.add(egui::Label::new(new_release));
-            }
+            // let new_release = "[NEW] Available version: ".to_owned() + &self.available_version;
+            // if self.is_new_update {
+            //     ui.add(egui::Label::new(new_release));
+            // }
         });
     }
 }
