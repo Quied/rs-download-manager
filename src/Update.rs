7438473fe06306ea6d450b6324b5b3c21c481e88
Quied <quiedcpp@gmail.com>
2024-02-05 23:34:06 +0200

    upd

--- src/Update.rs
+++ src/Update.rs
@@ -1,40 +1,63 @@
 pub mod update {
-    use self_update::cargo_crate_version;
-    use self_update::self_replace;
-    use std::io::{stdin, Read};
-    use std::{fs, io};
-
-    use self_update::{Download, Extract};
+    use egui::Response;
     use std::fs::{set_permissions, File, OpenOptions};
     use std::path::Path;
-
+    use std::{
+        fs,
+        io::{stdin, Read},
+    };
+    // use reqwest::
+    use self_replace::*;
     use std::env::consts::EXE_EXTENSION;
 
     pub fn check_updates() -> Result<(), Box<dyn (::std::error::Error)>> {
-
-       Ok(())
+        Ok(())
     }
 
+    pub fn self_delete() -> Result<(), Box<dyn std::error::Error>> {
+        self_replace::self_delete()?;
 
+        Ok(())
+    }
 
     pub fn replace_itself() -> Result<(), Box<dyn std::error::Error>> {
-        
         let exe = std::env::current_exe().unwrap();
         let new_executable = std::fs::read_link(exe.clone())
             .unwrap_or(exe)
-            .with_file_name("hello")
+            .with_file_name("Achernar")
             .with_extension(EXE_EXTENSION);
-    
+
         if !new_executable.is_file() {
             eprintln!("hello does not exist, run cargo build --example hello first.");
             std::process::exit(1);
         }
-    
+
         println!("Next time I run, I am the hello executable");
         self_replace::self_replace(&new_executable).unwrap();
-    
+
         if std::env::var("FORCE_EXIT").ok().as_deref() == Some("1") {
             std::process::exit(0);
+        }
+
+        Ok(())
+    }
+
+    pub fn server_reqwest(url: String) -> Result<(), Box<dyn std::error::Error>> {
+        let client = reqwest::blocking::Client::new();
+        let mut responce = client.get(&url).send()?;
+
+
+        if responce.status().is_success() {
+            let mut new_file = std::fs::File::create("downloaded_archive.tar.gz")?;
+
+            responce.copy_to(&mut new_file);
+            dbg!("Download...");
+
+            let archive_path = std::path::Path::new("./downloaded_archive.tar.gz");
+            try_decompress_tar(archive_path);
+
+        } else {
+            dbg!("Reqwest error:  {}", responce.status());
         }
 
         Ok(())
@@ -55,5 +78,4 @@ pub mod update {
         println!("[success decompressed] {:?}", target);
         Ok(())
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,19 @@
 use eframe::egui;
-use self_update::update::Release;
-use self_update::update::ReleaseUpdate;
-use webbrowser;
 
 mod Update;
 mod View;
 
-use Update::update::{check_updates, replace_itself};
-
-use egui::widgets::ProgressBar;
-use std::fs;
-// use std::env::consts::EXE_EXTENSION;
+use Update::update::{check_updates, replace_itself, self_delete, server_reqwest};
 
 // #[derive(Default)]
 struct MyApp {
-    allowed_to_close: bool,
-    allow_to_download: bool,
-    available_version: String,
-    actual_version: String,
-    is_new_update: bool,
-    update_list: Vec<String>,
-    all_releases: Vec<Release>,
+    current_version: String
 }
 
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            allow_to_download: true,
-            allowed_to_close: true,
-            available_version: String::from("0.1.0"),
-            update_list: Vec::new(),
-            is_new_update: false,
-            all_releases: Vec::new(),
-            actual_version: "0.0.0".to_owned(),
+            current_version: "v0.1.0".to_owned(),
         }
     }
 }
@@ -55,10 +36,12 @@ fn main() {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let APP_VERSION = "Current version 0.1.0".to_owned(); // HARD
-
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.horizontal(|ui| {
+
+                ui.add(egui::Label::new(&self.current_version));
+                ui.separator();
+
                 if ui.add(egui::Button::new("Check updates")).clicked() {
                     let _ = check_updates();
                 }
@@ -66,40 +49,23 @@ impl eframe::App for MyApp {
                 if ui.add(egui::Button::new("Replace itself")).clicked() {
                     let _ = replace_itself();
                 }
+
+                if ui.add(egui::Button::new("Self delete")).clicked() {
+                    let _ = self_delete();
+                }
+
+                if ui.add(egui::Button::new("Server Reqwest")).clicked() {
+                    let url = format!("http://localhost:3002/local");
+
+                    let _ = server_reqwest(url);
+                }
             });
 
             ui.separator();
 
             ui.vertical(|ui| {
                 ui.add(egui::Label::new("List updates:"));
-
-                // for release in &self.all_releases {
-                //     let version = &release.version;
-                //     ui.add(egui::Label::new(version.to_owned()));
-                // }
-
-                // if ui.add(egui::Button::new("Refresh")).clicked() {
-                //     self.all_releases = get_releases_list().expect("Error: ");
-                //     println!("REFRESH: {:?}", self.all_releases);
-                // }
-
-                // ui.separator();
-
-                // // === SHOW VERSION ===
-                // if !self.all_releases.is_empty() {
-                //     let now_release = &self.all_releases.last();
-                //     ui.add(egui::Label::new(&now_release.unwrap().version));
-                // }
-
-                // if ui.add(egui::Button::new("Update to last")).clicked() {
-                //     let _ = update_current();
-                // }
             });
-
-            // let new_release = "[NEW] Available version: ".to_owned() + &self.available_version;
-            // if self.is_new_update {
-            //     ui.add(egui::Label::new(new_release));
-            // }
         });
     }
 }
